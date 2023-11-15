1d7f67cdb51009b5b9c50cce0247b29000efceb1
Kuri3 <dev@kuri3.cc>
2023-11-16 04:00:39 +0900

    テスト

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -43,52 +43,6 @@ export default async function OGImage(req: NextRequest) {
     interBoldFontP
   ])
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          justifyContent: "center",
-          backgroundImage: `url(${pageInfo.image})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          fontWeight: 600,
-          color: "white",
-        }}
-      >
-        <h1
-          style={{
-            position: "absolute",
-            bottom: 60,
-            left: 80,
-            margin: 0,
-            fontSize: 50,
-            fontFamily: "Inter",
-            maxWidth: 900,
-            whiteSpace: "pre-wrap",
-            letterSpacing: -1,
-          }}
-        >
-          {pageInfo.title}
-        </h1>
-      </div>
-    ),
-    {
-      width: 1050,
-      height: 549,
-      fonts: [
-        {
-          name: "Inter",
-          data: interRegularFont,
-        },
-      ],
-    }
-  )
-
   // return new ImageResponse(
   //   (
   //     <div
