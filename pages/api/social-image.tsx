32a7cd64bfb9b353b70e15a651252d6998259404
Kuri3 <dev@kuri3.cc>
2023-11-16 03:57:12 +0900

    OGテスト

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -47,133 +47,179 @@ export default async function OGImage(req: NextRequest) {
     (
       <div
         style={{
-          position: 'relative',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          backgroundColor: '#1F2027',
-          alignItems: 'center',
-          justifyContent: 'center',
-          fontFamily: '"Inter", sans-serif',
-          color: 'black'
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          backgroundImage: `url(${pageInfo.image})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          fontWeight: 600,
+          color: "white",
         }}
       >
-        {pageInfo.image && (
-          <img
-            src={pageInfo.image}
-            style={{
-              position: 'absolute',
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover'
-              // TODO: satori doesn't support background-size: cover and seems to
-              // have inconsistent support for filter + transform to get rid of the
-              // blurred edges. For now, we'll go without a blur filter on the
-              // background, but Satori is still very new, so hopefully we can re-add
-              // the blur soon.
-
-              // backgroundImage: pageInfo.image
-              //   ? `url(${pageInfo.image})`
-              //   : undefined,
-              // backgroundSize: '100% 100%'
-              // TODO: pageInfo.imageObjectPosition
-              // filter: 'blur(8px)'
-              // transform: 'scale(1.05)'
-            }}
-          />
-        )}
-
-        <div
+        <h1
           style={{
-            position: 'relative',
-            width: 900,
-            height: 465,
-            display: 'flex',
-            flexDirection: 'column',
-            border: '16px solid rgba(0,0,0,0.3)',
-            borderRadius: 8,
-            zIndex: '1'
+            position: "absolute",
+            bottom: 60,
+            left: 80,
+            margin: 0,
+            fontSize: 50,
+            fontFamily: "Inter",
+            maxWidth: 900,
+            whiteSpace: "pre-wrap",
+            letterSpacing: -1,
           }}
         >
-          <div
-            style={{
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'space-around',
-              backgroundColor: '#fff',
-              padding: 24,
-              alignItems: 'center',
-              textAlign: 'center'
-            }}
-          >
-            {pageInfo.detail && (
-              <div style={{ fontSize: 32, opacity: 0 }}>{pageInfo.detail}</div>
-            )}
-
-            <div
-              style={{
-                fontSize: 70,
-                fontWeight: 700,
-                fontFamily: 'Inter'
-              }}
-            >
-              {pageInfo.title}
-            </div>
-
-            {pageInfo.detail && (
-              <div style={{ fontSize: 32, opacity: 0.6 }}>
-                {pageInfo.detail}
-              </div>
-            )}
-          </div>
-        </div>
-
-        {pageInfo.authorImage && (
-          <div
-            style={{
-              position: 'absolute',
-              top: 47,
-              left: 104,
-              height: 128,
-              width: 128,
-              display: 'flex',
-              borderRadius: '50%',
-              border: '4px solid #fff',
-              zIndex: '5'
-            }}
-          >
-            <img
-              src={pageInfo.authorImage}
-              style={{
-                width: '100%',
-                height: '100%'
-                // transform: 'scale(1.04)'
-              }}
-            />
-          </div>
-        )}
+          {pageInfo.title}
+        </h1>
       </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: 1050,
+      height: 549,
       fonts: [
         {
-          name: 'Inter',
+          name: "Inter",
           data: interRegularFont,
-          style: 'normal',
-          weight: 400
         },
-        {
-          name: 'Inter',
-          data: interBoldFont,
-          style: 'normal',
-          weight: 700
-        }
-      ]
+      ],
     }
   )
+
+  // return new ImageResponse(
+  //   (
+  //     <div
+  //       style={{
+  //         position: 'relative',
+  //         width: '100%',
+  //         height: '100%',
+  //         display: 'flex',
+  //         flexDirection: 'column',
+  //         backgroundColor: '#1F2027',
+  //         alignItems: 'center',
+  //         justifyContent: 'center',
+  //         fontFamily: '"Inter", sans-serif',
+  //         color: 'black'
+  //       }}
+  //     >
+  //       {pageInfo.image && (
+  //         <img
+  //           src={pageInfo.image}
+  //           style={{
+  //             position: 'absolute',
+  //             width: '100%',
+  //             height: '100%',
+  //             objectFit: 'cover'
+  //             // TODO: satori doesn't support background-size: cover and seems to
+  //             // have inconsistent support for filter + transform to get rid of the
+  //             // blurred edges. For now, we'll go without a blur filter on the
+  //             // background, but Satori is still very new, so hopefully we can re-add
+  //             // the blur soon.
+
+  //             // backgroundImage: pageInfo.image
+  //             //   ? `url(${pageInfo.image})`
+  //             //   : undefined,
+  //             // backgroundSize: '100% 100%'
+  //             // TODO: pageInfo.imageObjectPosition
+  //             // filter: 'blur(8px)'
+  //             // transform: 'scale(1.05)'
+  //           }}
+  //         />
+  //       )}
+
+  //       <div
+  //         style={{
+  //           position: 'relative',
+  //           width: 900,
+  //           height: 465,
+  //           display: 'flex',
+  //           flexDirection: 'column',
+  //           border: '16px solid rgba(0,0,0,0.3)',
+  //           borderRadius: 8,
+  //           zIndex: '1'
+  //         }}
+  //       >
+  //         <div
+  //           style={{
+  //             width: '100%',
+  //             height: '100%',
+  //             display: 'flex',
+  //             flexDirection: 'column',
+  //             justifyContent: 'space-around',
+  //             backgroundColor: '#fff',
+  //             padding: 24,
+  //             alignItems: 'center',
+  //             textAlign: 'center'
+  //           }}
+  //         >
+  //           {pageInfo.detail && (
+  //             <div style={{ fontSize: 32, opacity: 0 }}>{pageInfo.detail}</div>
+  //           )}
+
+  //           <div
+  //             style={{
+  //               fontSize: 70,
+  //               fontWeight: 700,
+  //               fontFamily: 'Inter'
+  //             }}
+  //           >
+  //             {pageInfo.title}
+  //           </div>
+
+  //           {pageInfo.detail && (
+  //             <div style={{ fontSize: 32, opacity: 0.6 }}>
+  //               {pageInfo.detail}
+  //             </div>
+  //           )}
+  //         </div>
+  //       </div>
+
+  //       {pageInfo.authorImage && (
+  //         <div
+  //           style={{
+  //             position: 'absolute',
+  //             top: 47,
+  //             left: 104,
+  //             height: 128,
+  //             width: 128,
+  //             display: 'flex',
+  //             borderRadius: '50%',
+  //             border: '4px solid #fff',
+  //             zIndex: '5'
+  //           }}
+  //         >
+  //           <img
+  //             src={pageInfo.authorImage}
+  //             style={{
+  //               width: '100%',
+  //               height: '100%'
+  //               // transform: 'scale(1.04)'
+  //             }}
+  //           />
+  //         </div>
+  //       )}
+  //     </div>
+  //   ),
+  //   {
+  //     width: 1200,
+  //     height: 630,
+  //     fonts: [
+  //       {
+  //         name: 'Inter',
+  //         data: interRegularFont,
+  //         style: 'normal',
+  //         weight: 400
+  //       },
+  //       {
+  //         name: 'Inter',
+  //         data: interBoldFont,
+  //         style: 'normal',
+  //         weight: 700
+  //       }
+  //     ]
+  //   }
+  // )
 }
